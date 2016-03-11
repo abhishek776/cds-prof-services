5d5519fd14beea67d73d525e807433e115f5f501
Diane Wang <dianewang@berkeley.edu>
2016-03-11 05:48:46 +0000

    basic user profile setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311012530) do
+ActiveRecord::Schema.define(version: 20160311042809) do
 
   create_table "dog_like_linkers", force: :cascade do |t|
     t.integer "dog_id"
@@ -66,12 +66,21 @@ ActiveRecord::Schema.define(version: 20160311012530) do
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
+    t.string   "image"
+    t.string   "phone_number"
+    t.string   "description"
+    t.string   "address"
+    t.string   "zipcode"
+    t.string   "city"
+    t.string   "country"
   end
 
 end
