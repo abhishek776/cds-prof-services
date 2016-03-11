82c3a251d2994760f7860fe9518d5798d17a2979
Abhishek Fatehpuria <abhishekf@berkeley.edu>
2016-03-11 23:47:52 +0000

    pre-merge

--- db/schema.rb
+++ db/schema.rb
@@ -61,12 +61,21 @@ ActiveRecord::Schema.define(version: 20160311093320) do
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
-    t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "gender"
+    t.string   "image"
+    t.string   "phone_number"
+    t.string   "description"
+    t.string   "address"
+    t.string   "zipcode"
+    t.string   "city"
+    t.string   "country"
   end
 
 end
