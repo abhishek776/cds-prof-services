7b4183985f7bbbbfe68220f8856b82f8e0904241
Abhishek Fatehpuria <abhishekf@berkeley.edu>
2016-03-11 22:50:02 +0000

    changing app id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,8 +6,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     app_id = "175967292784758"
     app_secret = "d1b2280c7bf6bb831189b22014f79136"
   else  
-    app_id = "175968132784674"
-    app_secret = "af66c76beb1e0fd9393b9f8024da6dff"
+    app_id = "176654776049343"
+    app_secret = "bc5088f8e4c76d6f6b1d710cd13406fe"
   end
 
   provider :facebook, app_id, app_secret, {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
