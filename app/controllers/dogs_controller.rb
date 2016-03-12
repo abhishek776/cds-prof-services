2b26ea4866a3e3a635e20bc95cf8a608d7a64415
erq1 <era212@yandex.com>
2016-03-12 11:23:48 +0000

    not tested add dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,19 +1,24 @@
 class DogsController < ApplicationController
   before_action :set_dog, only: [:show, :edit, :update, :destroy]
+  
 
   # GET /dogs
   # GET /dogs.json
   def index
+    @user = User.find(session[:user_id])
     @dogs = Dog.all
   end
 
   # GET /dogs/1
   # GET /dogs/1.json
   def show
+    # id = params[:name] # retrieve movie ID from URI route
+    # @dog = Dog.find(id)
   end
 
   # GET /dogs/new
   def new
+    @user = User.find(session[:user_id])
     @dog = Dog.new
   end
 
@@ -23,18 +28,28 @@ class DogsController < ApplicationController
 
   # POST /dogs
   # POST /dogs.json
+  # def create
+  #   @dog = Dog.new(dog_params)
+
+  #   respond_to do |format|
+  #     if @dog.save
+  #       format.html { redirect_to @dog, notice: 'Dog was successfully created.' }
+  #       format.json { render :show, status: :created, location: @dog }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @dog.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  
   def create
     @dog = Dog.new(dog_params)
-
-    respond_to do |format|
-      if @dog.save
-        format.html { redirect_to @dog, notice: 'Dog was successfully created.' }
-        format.json { render :show, status: :created, location: @dog }
-      else
-        format.html { render :new }
-        format.json { render json: @dog.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = User.find(session[:user_id])
+    if @dog.save
+      redirect_to @user
+    else
+      render 'new'
+    end 
   end
 
   # PATCH/PUT /dogs/1
@@ -69,6 +84,7 @@ class DogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def dog_params
-      params.fetch(:dog, {})
+      # params.fetch(:dog, :name)
+      params.require(:dog).permit(:name, :user_id)
     end
 end
