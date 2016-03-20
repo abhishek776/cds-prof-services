09002ceff24465ca97ee11d834c469dee18fb6cb
erq1 <era212@yandex.com>
2016-03-20 05:58:53 +0000

    rspec cleaned

--- spec/factories/stars.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :star do
-    user nil
-starred nil
-  end
-
-end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,18 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    first_name "Eric"
-    last_name "Husk"
-    gender "Male"
-    status "Looking"
-    phone_number "(123)123-1234"
-    email "eric@husk.com"
-    availability "All the time"
-    description "Hello everyone"
-    address "387 Soda Hall"
-    zipcode "94720"
-    city "Berkeley"
-    country "US"
-    id 1
-    uid "12345"
+    provider "MyString"
+    uid "MyString"
+    name "MyString"
+    oauth_token "MyString"
+    oauth_expires_at "2016-03-11 05:25:21"
   end
 end
