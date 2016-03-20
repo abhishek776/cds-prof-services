dfebd0306a4acb4a08e8469dde176149d7dccfd3
erq1 <era212@yandex.com>
2016-03-20 04:20:01 +0000

    deleted bad migrationss

--- db/migrate/20160319032542_create_pers_linkers.rb
+++
@@ -1,8 +0,0 @@
-class CreatePersLinkers < ActiveRecord::Migration
-  def change
-    create_table :dog_personality_linkers do |t|
-      t.integer :dog_id
-      t.integer :personality_id
-    end
-  end
-end
