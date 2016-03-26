ca23689e42f03545648aace2cd1ecac6ae98e3c1
erq1 <era212@yandex.com>
2016-03-26 09:10:41 +0000

    fixed images, 94 coverage dor dogs_controller

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -11,16 +11,16 @@ class Dog < ActiveRecord::Base
   belongs_to :energy_level
   belongs_to :size
   
-  # has_attached_file :image,
-  #   :styles => {
-  #     :large =>"500x500>",
-  #     :medium => "300x300>",
-  #     :thumb => "100x100>" },
-  #   :url => "/:class/:attachment/:id/:style_:basename.:extension"
+  has_attached_file :image,
+    :styles => {
+      :large =>"500x500>",
+      :medium => "300x300>",
+      :thumb => "100x100>" },
+    :url => "/:class/:attachment/:id/:style_:basename.:extension"
   
-  # validates_attachment :image, 
-  #     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] },
-  #     :size => { :in => 0..500.kilobytes } 
+  validates_attachment :image, 
+      content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] },
+      :size => { :in => 0..500.kilobytes } 
 
 
 
