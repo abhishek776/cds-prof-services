40f5d1b7713cc6b4c91ede506059010d9e732c69
erq1 <era212@yandex.com>
2016-04-01 06:34:52 +0000

    merged stars,events,mixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,10 +50,10 @@ class UsersController < ApplicationController
       flash[:notice] = "You may only edit your own profile."
       redirect_to @current_user
     elsif params[:user] != nil and @current_user.update_attributes(user_params)
-      @current_user.dogs.each do |dog|
-        dog.geocode
-        dog.save
-      end
+      # @current_user.dogs.each do |dog|
+      #   dog.geocode
+      #   dog.save
+      # end
       flash[:notice] = "Profile successfully updated."
       redirect_to @current_user
     else
