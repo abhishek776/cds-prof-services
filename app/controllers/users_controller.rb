de3f02076ccb00c3b0306db39c53c0ee15db35b8
Mykhaylo Krutchenko <krutchm@gmail.com>
2016-04-16 23:14:25 +0000

    uid to id changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       id = params[:id]
       @own_profile = false
       @user = User.find(id)
-      @dogs = Dog.where(user_id: @user.uid)
+      @dogs = Dog.where(user_id: @user.id)
       
       if @user == @current_user
         @own_profile = true
